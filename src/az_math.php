fc3af8e6e5fafc679695579545988b8b4d740738
azrideus <mehrdadtavangar@gmail.com>
2025-02-07 18:20:12 +0300

    added has_fraction and digits

--- src/az_math.php
+++ src/az_math.php
@@ -17,4 +17,14 @@ class az_math
 		$reg = $value + 0.5 / (10 ** $precision);
 		return round($reg, $precision, $reg > 0 ? PHP_ROUND_HALF_DOWN : PHP_ROUND_HALF_UP);
 	}
+	public static function digits(int|float $number): int
+	{
+		$digits = str_split(strval($number));
+		$digitCount = count($digits);
+		return $digitCount;
+	}
+	public static function has_fraction(int|float $number): bool
+	{
+		return in_array(str_split(strval($number)), ['.', ',']);
+	}
 }
