996634225c39e4370ffc172bcaa74a18fc980a9f
Azrideus <22293394+Azrideus@users.noreply.github.com>
2025-02-20 17:02:50 +0300

    better compressed_name and fix register

--- src/wp/az_wp_styles.php
+++ src/wp/az_wp_styles.php
@@ -15,12 +15,9 @@ trait az_wp_styles
 	) {
 		$pdir = az_wp::getPluginDir($plugin_file);
 		$cache_folder = az_string::join_paths($pdir, 'cache');
+		$cache_folder_url = az_string::join_paths(az_wp::getPluginUrl($plugin_file), 'cache');
 
-		$url_path = az_string::join_url(
-			az_wp::getPluginUrl($plugin_file),
-			'src/styles/',
-			$folder
-		);
+
 		$file_path = az_string::join_paths(
 			$pdir,
 			'src/styles/',
@@ -32,7 +29,7 @@ trait az_wp_styles
 			mkdir($cache_folder, 0777, true);
 		}
 		/* -------------------------------------------------------------------------- */
-		$compressed_name = '__' . $plugin_name . '_' . $folder . '_' . $version . '.css';
+		$compressed_name = '__styles_' . $plugin_name . '_' . $folder . '_' . $version . '.css';
 		$compressed_file = az_string::join_paths($cache_folder, $compressed_name);
 		if (!\file_exists($compressed_file)) {
 			$css_files = scandir($file_path);
@@ -50,14 +47,13 @@ trait az_wp_styles
 			fwrite($myfile, $compressed_css);
 			fclose($myfile);
 		}
-		$style_name = 'az_style__' . $compressed_name;
 		wp_enqueue_style(
-			$style_name,
-			$compressed_file,
+			$compressed_name,
+			az_string::join_url($cache_folder_url, $compressed_name),
 			$deps,
 			$version
 		);
-		return $style_name;
+		return $compressed_name;
 	}
 	static function compress_css($input)
 	{
