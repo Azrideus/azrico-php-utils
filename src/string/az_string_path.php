6cbbfcc56204f93ef4e8e3644ca6b50aa72569f7
azrideus <mehrdadtavangar@gmail.com>
2025-02-26 12:54:56 +0300

    added sanitize_filename

--- src/string/az_string_path.php
+++ src/string/az_string_path.php
@@ -5,6 +5,18 @@ namespace AzUtils\string;
 trait az_string_path
 {
 
+	static function sanitize_filename($file)
+	{
+		// Remove anything which isn't a word, whitespace, number
+		// or any of the following caracters -_~,;[]().
+		// If you don't need to handle multi-byte characters
+		// you can use preg_replace rather than mb_ereg_replace
+		// Thanks @Łukasz Rysiak!
+		$file = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $file);
+		// Remove any runs of periods (thanks falstro!)
+		$file = mb_ereg_replace("([\.]{2,})", '', $file);
+		return $file;
+	}
 	static function fix_path($p, $sep = '')
 	{
 		if (empty($sep)) $sep = DIRECTORY_SEPARATOR;
