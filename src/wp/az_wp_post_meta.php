38eb21b7f28ec1fea63b0a956b2133e9b685afa3
Azrideus <mehrdadtavangar@gmail.com>
2025-03-02 01:59:45 +0300

    better get_meta_of

--- src/wp/az_wp_post_meta.php
+++ src/wp/az_wp_post_meta.php
@@ -86,34 +86,27 @@ trait az_wp_post_meta
 		assert(is_string($key) && strlen($key) > 0, 'invalid key for meta! got: ' . strval($key));
 		assert(function_exists('get_post_meta'), 'get_post_meta function is not defined. are you in a wordpress environment?');
 
-		if (
-			is_object($search)
-			&& property_exists($search, 'field')
-			&& is_a($search->field, 'WP_Post')
-		) {
-			$search = $search->field;
-		}
 
-
-		/**
-		 * WP_Post
-		 */
-		if (is_a($search, 'WP_Post')) {
-			$search = $search->ID;
-		}
 		/**
 		 * WP_Term
 		 */
-		else if (
-			is_a($search, 'WP_Term')
+		if (
+			$search instanceof \WP_Term
 		) {
 			return get_term_meta($search->term_id, $key, true);
 		}
 		/**
 		 * WC_Order_Item
 		 */
-		else if (is_a($search, 'WC_Order_Item')) {
+		if ($search instanceof \WC_Order_Item) {
 			return $search->get_meta($key);
+		}
+		/**
+		 * WP_Post
+		 */
+		$search = az_wp::get_post($search);
+		if ($search instanceof \WP_Post) {
+			$search = $search->ID;
 		}
 
 		assert(is_numeric($search), 'could not load the post id to get its meta! got: ' . strval($search));
@@ -126,11 +119,11 @@ trait az_wp_post_meta
 	}
 	static function set_meta_of($search, string $key, $value)
 	{
-		$search = az_wp::get_post($search);
 		/**
 		 * WP_Post
 		 */
-		if (is_a($search, 'WP_Post')) {
+		$search = az_wp::get_post($search);
+		if ($search instanceof \WP_Post) {
 			$search = $search->ID;
 		}
 		assert(is_numeric($search), 'could not load the post id to set its meta! got: ' . strval($search));
