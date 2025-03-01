d98846e2542306d6b05bc57b4a44408c9d2ef0fb
Azrideus <mehrdadtavangar@gmail.com>
2025-03-01 13:31:50 +0300

    added math is_satisfied

--- src/az_math.php
+++ src/az_math.php
@@ -27,4 +27,53 @@ class az_math
 	{
 		return in_array(str_split(strval($number)), ['.', ',']);
 	}
+	/**
+	 * check if a basic `>,<,<=,>=` operation is satisfied
+	 *
+	 * @param string $operation
+	 * @return boolean
+	 */
+	public static function is_satisfied(string $operation): bool
+	{
+		$operator_regex = "/(>=|<=|<|>)/";
+
+		// Extract operands and operator
+		preg_match($operator_regex, $operation, $operator_match);
+
+		if (!$operator_match) {
+			throw new \InvalidArgumentException("Invalid operation format");
+		}
+
+		$operator = $operator_match[0];
+		$operands = preg_split($operator_regex, $operation);
+
+		if (count($operands) !== 2) {
+			throw new \InvalidArgumentException("Invalid number of operands");
+		}
+
+		// Trim and convert operands to numbers
+		$left = trim($operands[0]);
+		$right = trim($operands[1]);
+
+		if (!is_numeric($left) || !is_numeric($right)) {
+			throw new \InvalidArgumentException("Operands must be numeric");
+		}
+
+		$left = (float) $left;
+		$right = (float) $right;
+
+		// Evaluate the operation
+		switch ($operator) {
+			case '>':
+				return $left > $right;
+			case '<':
+				return $left < $right;
+			case '>=':
+				return $left >= $right;
+			case '<=':
+				return $left <= $right;
+			default:
+				throw new \InvalidArgumentException("Unknown operator");
+		}
+	}
 }
