de1276650f0351cf61e46cd0792eb109abc81e9c
Azrideus <mehrdadtavangar@gmail.com>
2025-05-04 21:17:22 +0300

    fixed get_post

--- src/wp/az_wp_post.php
+++ src/wp/az_wp_post.php
@@ -110,7 +110,7 @@ trait az_wp_post
 	 */
 	static function get_post($search, string|array $post_type = ''): \WP_Post|null
 	{
-		$postlist = self::findPosts($search, $post_type, 1);
+		$postlist = static::get_post_list($search, $post_type, 1);
 		return end($postlist);
 	}
 	/**
