6f2588353dad75de6f24387e62fdb8e4941bd81e
Azrideus <mehrdadtavangar@gmail.com>
2025-08-14 10:46:55 +0300

    wc_format_currency

--- src/wp/az_wp_orders.php
+++ src/wp/az_wp_orders.php
@@ -78,4 +78,65 @@ trait az_wp_orders
 		}
 		return null;
 	}
+
+	/**
+	 * add currency of the order to the given number
+	 */
+	static function wc_format_currency(\WC_Order|int $order, mixed $input_number, $precision = 2): string
+	{
+		if (! \function_exists('get_woocommerce_currency')) {
+			return number_format((float) $input_number, 2, '.', ',');
+		}
+		// Normalize order
+		if (is_numeric($order))
+			$order = wc_get_order($order);
+
+		if (!($order instanceof \WC_Order)) {
+			// Fallback to store currency if order is invalid
+			$currency = get_woocommerce_currency();
+		} else {
+			$currency = $order->get_currency() ?: get_woocommerce_currency();
+		}
+
+		$decimals      = wc_get_price_decimals();
+		$decimal_sep   = wc_get_price_decimal_separator();
+		$thousand_sep  = wc_get_price_thousand_separator();
+		$symbol        = get_woocommerce_currency_symbol($currency);
+		$position      = get_option('woocommerce_currency_pos', 'left');
+
+		// Normalize and format number
+		$amount = wc_format_decimal($amount, $decimals);
+		$is_negative = ((float) $amount) < 0;
+		$abs_amount  = abs((float) $amount);
+		$num         = number_format($abs_amount, $decimals, $decimal_sep, $thousand_sep);
+
+		if ($decimals > 0) {
+			// Remove trailing zeros per Woo formatting style
+			$num = preg_replace('/(' . preg_quote($decimal_sep, '/') . '\d*?)0+$/', '$1', $num);
+			$num = rtrim($num, $decimal_sep);
+		}
+		// Assemble by position (no HTML)
+		switch ($position) {
+			case 'left':
+				$out = $symbol . $num;
+				break;
+			case 'right':
+				$out = $num . $symbol;
+				break;
+			case 'left_space':
+				$out = $symbol . ' ' . $num;
+				break;
+			case 'right_space':
+				$out = $num . ' ' . $symbol;
+				break;
+			default:
+				$out = $symbol . $num;
+		}
+
+		if ($is_negative) {
+			// Match Woo style: minus before everything
+			$out = '-' . $out;
+		}
+		return apply_filters('az_currency_format', $out, $order, $input_number, $precision);
+	}
 }
